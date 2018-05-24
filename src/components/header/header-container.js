10da50c8ed2103e8da70a4f7894388231b138d6e
Tanya <mellene@gmail.com>
2018-05-24 15:50:55 -0600

    login form

--- src/components/header/header-container.js
+++ src/components/header/header-container.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './header-container.css'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+// const Header = () => (
+//   <h1>
+//     Header
+//   </h1>
+// )
+class Header extends Component {
+  onLogoutclick = (event) => {
+    event.preventDefault()
+    this.props.logout()
+  }
+  render() {
+    return(
+      <div className='header-container'>
+        <a className='title' href='#title'>Alicia's Todo</a>
+        { this.props.username !== null && <a href='#logout' onClick={this.onLogoutClick}>{`Logout ${this.props.username}`}</a>}
+      </div>
+    )
+  }
+}
 
-const Header = () => (
-  <h1>
-    Header
-  </h1>
-)
-
+const mapStateToProps = (state) => state.auth
+//const mapDispatchToProps = (dispatch) => bindActionCreators({logout}, dispatch)
 export default Header
